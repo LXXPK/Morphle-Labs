0a4c3a64af4380b71265c7aaa81e13e5b452433b
B_Kiran <124519398+LXXPK@users.noreply.github.com>
2024-10-19 08:06:01 +0000

    complete

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+from datetime import datetime
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/htop')
+def htop():
+    name = "B Kiran"
+    username = os.getlogin()
+    server_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    top_output = subprocess.getoutput('top -b -n1 | head -15')
+    return f"Name: {name}<br>Username: {username}<br>Server Time (IST): {server_time}<br><pre>{top_output}</pre>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
