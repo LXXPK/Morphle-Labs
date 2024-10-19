8206c9c1144060ba13deac5010b882e59db4e71b
B_Kiran <124519398+LXXPK@users.noreply.github.com>
2024-10-19 14:31:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 @app.route('/htop')
 def htop():
-    name = "B Kiran"  # Change this as per your requirement
-    username = os.getlogin()  # This works on Windows too
+    name = "B Kiran"  
+    username = os.getlogin() 
     server_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    # Use 'tasklist' command to list running processes on Windows
+    top_output = subprocess.getoutput('top -b -n1 | head -15')
     tasklist_output = subprocess.getoutput('tasklist')
 
-    # Return the output as HTML
+   
     return f"Name: {name}<br>Username: {username}<br>Server Time (IST): {server_time}<br><pre>{tasklist_output}</pre>"
 
 if __name__ == "__main__":
